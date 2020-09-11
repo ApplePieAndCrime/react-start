1fe5b57542065672ebae049144f8015cb6cb775b
ApplePieAndCrime <33925666+ApplePieAndCrime@users.noreply.github.com>
2020-09-11 17:56:31 +0300

    Updates

---
+++ precache-manifest.0daf69be95627eb2a3452abd7f70163b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e36ed002d826091424602b7218ff4e8",
+    "url": "/react-start/index.html"
+  },
+  {
+    "revision": "2abc3715bbc4718eacad",
+    "url": "/react-start/static/css/main.613dd26d.chunk.css"
+  },
+  {
+    "revision": "6f3c1738aefe9fc4d9c3",
+    "url": "/react-start/static/js/2.81fd5fc8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-start/static/js/2.81fd5fc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2abc3715bbc4718eacad",
+    "url": "/react-start/static/js/main.bec8963c.chunk.js"
+  },
+  {
+    "revision": "8298e145ff82bb292146",
+    "url": "/react-start/static/js/runtime-main.a485bc31.js"
+  }
+]);
